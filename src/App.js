ee69302432694252279f40f1fe78fe285b542e09
Yeferzon Quisoboni <daavid@misena.edu.co>
2023-05-01 21:09:39 -0500

    Se configura correctamente rutas con redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import Error404 from "containers/errors/error404";
+import Home from "containers/pages/Home";
+import store from "store";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
 function App() {
   return (
-    <div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          {}
+          <Route path="*" element={<Error404/>}/>
+
+          {}
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </Router>
       
-    </div>
+    </Provider>
   );
 }
 
